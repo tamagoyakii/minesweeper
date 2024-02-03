8dc530bd134ec06dd515975c9b8339878373a8d0
tamagoyakii <rlawlgus4063@naver.com>
2024-02-04 00:06:06 +0900

    [feat] 남은 지뢰 표시 구현

--- src/features/BombCounter.tsx
+++ src/features/BombCounter.tsx
@@ -1,5 +1,12 @@
+import { useSelector } from 'react-redux';
+
+import { RootState } from 'src/app/store';
 import NumberPad from 'src/features/NumberPad';
 
 export default function BombCounter() {
-  return <NumberPad value={0} />;
+  const remainingBombs = useSelector(
+    (state: RootState) => state.game.remainingBombs
+  );
+
+  return <NumberPad value={remainingBombs} />;
 }
