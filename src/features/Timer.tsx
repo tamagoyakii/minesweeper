47ce265065dbc427bb5dbd9ad5e2b5fc2062cb2b
tamagoyakii <rlawlgus4063@naver.com>
2024-02-03 22:36:16 +0900

    [feat] NumberPad를 활용한 타이머 컴포넌트 구현

---
+++ src/features/Timer.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from 'src/app/store';
+import NumberPad from './NumberPad';
+
+export default function Timer() {
+  const isPlaying = useSelector((state: RootState) => state.game.isPlaying);
+  const [second, setSecond] = useState(0);
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+    if (isPlaying) {
+      interval = setInterval(() => {
+        setSecond((prev) => {
+          if (prev < 999) return prev + 1;
+          return prev;
+        });
+      }, 1000);
+    }
+    if (!isPlaying) setSecond(0);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isPlaying]);
+
+  return <NumberPad value={second} />;
+}
